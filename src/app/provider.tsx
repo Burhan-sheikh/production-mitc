27c0b665fa6e1d067b0f4ad04825fd0c47a2a74d
Burhan-sheikh <epithet02@gmail.com>
2025-12-28 20:23:58 +0530

    Update: Add visit tracking to provider

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -2,12 +2,19 @@
 
 import { AuthProvider } from '@/lib/auth-context';
 import { Toaster } from 'react-hot-toast';
+import { useVisitTracking } from '@/hooks/useVisitTracking';
+
+function VisitTracker() {
+  useVisitTracking();
+  return null;
+}
 
 export function Provider({ children }: { children: React.ReactNode }) {
   return (
     <AuthProvider>
+      <VisitTracker />
       {children}
-      <Toaster position="top-right" />
+      <Toaster position="bottom-right" />
     </AuthProvider>
   );
 }
